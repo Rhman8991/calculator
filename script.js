8a7a5e71a42da0348b5983fcc45b4c814b9e9bea
rahman <rahman17wr@gmail.com>
2024-08-06 12:26:57 +0700

    Define function for math operation

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const add = (first, second) => first + second;
+    const substract = (first, second) => first - second;
+    const multiply = (first, second) => first * second;
+    const divide = (first, second) => first / second;
+    function operate (op, first, second) {
+        switch (op) {
+            case "+":
+                return add(first, second);
+            case "-":
+                return substract(first, second);
+            case "*":
+                return multiply(first, second);
+            case "/":
+                return divide(first, second);
+        }
+    }
+})
